e45cd7323cac5594cd8d300b1ba41c59cf07804f
nikolaj <nikvkov@gmail.com>
2018-05-23 15:23:20 +0300

    Добавьте файлы проекта.

---
+++ ExsalesMobileApp/ExsalesMobileApp/pages/StartPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExsalesMobileApp.pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+		public StartPage ()
+		{
+			InitializeComponent ();
+
+            //инициализация элементов формы
+            logo_image.Source = @"logo.jpg";
+
+            //данные локализации
+            en_login.Placeholder = "Login";
+            en_password.Placeholder = "Password";
+            bt_sign_in.Text = "Sign In";
+            label_register.Text = "Create new account?";
+            bt_register.Text = "Register";
+
+        }//c_tor
+	}//class
+}//namespace
